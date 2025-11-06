983ee6352ef0f987824ee9d42664f166405eb02d
PasteK <desousafreitasalexis@gmail.com>
2025-11-06 02:17:05 +0100

    added monsters

--- src/app/game/classes/monsters/simpleMonster.ts
+++ src/app/game/classes/monsters/simpleMonster.ts
@@ -75,94 +75,94 @@ export const MONSTER_DEFINITIONS: MonsterConfig[] = [
             max: 5
         },
     },
-    // {
-    //     name: 'piloteRaccoon',
-    //     texture: 'piloteRaccoon',
-    //     maxHP: 450,
-    //     actions: [
-    //         { type: 'attack', value: 5, description: 'TA !' },
-    //     ],
-    //     goldReward: {
-    //         min: 2,
-    //         max: 5
-    //     },
-    //     actionsPerTurn: 1,
-    //     sounds: {
-    //         spawn: 'sfx_piloteRaccoon_spawn',
-    //         // action: {
-    //         //     attack: 'sfx_piloteRaccoon_attack',
-    //         // },
-    //         hit: ['sfx_piloteRaccoon_hit1', 
-    //             'sfx_piloteRaccoon_hit2', 
-    //             'sfx_piloteRaccoon_hit3',
-    //             'sfx_piloteRaccoon_hit4',
-    //             'sfx_piloteRaccoon_hit5',
-    //             'sfx_piloteRaccoon_hit6',
-    //             'sfx_piloteRaccoon_hit7',
-    //             'sfx_piloteRaccoon_hit8',
-    //             'sfx_piloteRaccoon_hit9',
-    //             'sfx_piloteRaccoon_hit10'],
-    //     }
-    // },
-    // {
-    //     name: 'maccaroni',
-    //     texture: 'maccaroni',
-    //     maxHP: 350,
-    //     actions: [
-    //         { type: 'combo', value: 0, description: 'PROJEEET', delayMs: 10, steps: [
-    //             { type: 'transform', value: 2, description: 'PROJET' },
-    //             { type: 'attack', value: 5, description: 'Attaque' },
-    //             { type: 'transform', value: 0, description: 'PluPROJET' },
+    {
+        name: 'piloteRaccoon',
+        texture: 'piloteRaccoon',
+        maxHP: 450,
+        actions: [
+            { type: 'attack', value: 5, description: 'TA !' },
+        ],
+        goldReward: {
+            min: 2,
+            max: 5
+        },
+        actionsPerTurn: 1,
+        sounds: {
+            spawn: 'sfx_piloteRaccoon_spawn',
+            // action: {
+            //     attack: 'sfx_piloteRaccoon_attack',
+            // },
+            hit: ['sfx_piloteRaccoon_hit1', 
+                'sfx_piloteRaccoon_hit2', 
+                'sfx_piloteRaccoon_hit3',
+                'sfx_piloteRaccoon_hit4',
+                'sfx_piloteRaccoon_hit5',
+                'sfx_piloteRaccoon_hit6',
+                'sfx_piloteRaccoon_hit7',
+                'sfx_piloteRaccoon_hit8',
+                'sfx_piloteRaccoon_hit9',
+                'sfx_piloteRaccoon_hit10'],
+        }
+    },
+    {
+        name: 'maccaroni',
+        texture: 'maccaroni',
+        maxHP: 350,
+        actions: [
+            { type: 'combo', value: 0, description: 'PROJEEET', delayMs: 10, steps: [
+                { type: 'transform', value: 2, description: 'PROJET' },
+                { type: 'attack', value: 5, description: 'Attaque' },
+                { type: 'transform', value: 0, description: 'PluPROJET' },
 
-    //             { type: 'transform', value: 2, description: 'PROJET' },
-    //             { type: 'attack', value: 5, description: 'Attaque' },
-    //             { type: 'transform', value: 0, description: 'PluPROJET' },
+                { type: 'transform', value: 2, description: 'PROJET' },
+                { type: 'attack', value: 5, description: 'Attaque' },
+                { type: 'transform', value: 0, description: 'PluPROJET' },
 
-    //             { type: 'transform', value: 2, description: 'PROJET' },
-    //             { type: 'attack', value: 5, description: 'Attaque' },
-    //             { type: 'transform', value: 0, description: 'PluPROJET' },
+                { type: 'transform', value: 2, description: 'PROJET' },
+                { type: 'attack', value: 5, description: 'Attaque' },
+                { type: 'transform', value: 0, description: 'PluPROJET' },
 
-    //             { type: 'transform', value: 2, description: 'PROJET' },
-    //             { type: 'attack', value: 5, description: 'Attaque' },
-    //             { type: 'transform', value: 0, description: 'PluPROJET' }
-    //         ]},
-    //         { type: 'StealPercent', value: 20, description: 'impots' },
-    //     ],
-    //     goldReward: {
-    //         min: 3,
-    //         max: 5
-    //     },
-    //     actionsPerTurn: 1,
-    //     sounds: {
-    //         // spawn: 'sfx_maccaroni_spawn',
-    //         action: {
-    //             attack: 'sfx_maccaroni_attack',
-    //         },
-    //         death: 'sfx_maccaroni_death',
-    //         hit: 'sfx_maccaroni_hit'
-    //     }
-    // },
-    // {
-    //     name: 'laitka',
-    //     texture: 'laitka',
-    //     maxHP: 250,
-    //     actions: [
-    //         { type: 'attack', value: 10, description: 'ATCHOUM' },
-    //         { type: 'milk', value: 50, description: 'MILK' },
-    //     ],
-    //     goldReward: {
-    //         min: 1,
-    //         max: 4
-    //     },
-    //     actionsPerTurn: 1,
-    //     sounds: {
-    //         spawn: 'sfx_laitka_spawn',
-    //         action: {
-    //             attack: 'sfx_laitka_attack',
-    //             milk: 'sfx_laitka_milk',
-    //         },
-    //         hit: 'sfx_laitka_hit',
-    //         death: 'sfx_laitka_death'
-    //     }
-    // }
+                { type: 'transform', value: 2, description: 'PROJET' },
+                { type: 'attack', value: 5, description: 'Attaque' },
+                { type: 'transform', value: 0, description: 'PluPROJET' }
+            ]},
+            { type: 'StealPercent', value: 20, description: 'impots' },
+        ],
+        goldReward: {
+            min: 3,
+            max: 5
+        },
+        actionsPerTurn: 1,
+        sounds: {
+            // spawn: 'sfx_maccaroni_spawn',
+            action: {
+                attack: 'sfx_maccaroni_attack',
+            },
+            death: 'sfx_maccaroni_death',
+            hit: 'sfx_maccaroni_hit'
+        }
+    },
+    {
+        name: 'laitka',
+        texture: 'laitka',
+        maxHP: 250,
+        actions: [
+            { type: 'attack', value: 10, description: 'ATCHOUM' },
+            { type: 'milk', value: 50, description: 'MILK' },
+        ],
+        goldReward: {
+            min: 1,
+            max: 4
+        },
+        actionsPerTurn: 1,
+        sounds: {
+            spawn: 'sfx_laitka_spawn',
+            action: {
+                attack: 'sfx_laitka_attack',
+                milk: 'sfx_laitka_milk',
+            },
+            hit: 'sfx_laitka_hit',
+            death: 'sfx_laitka_death'
+        }
+    }
 ]
